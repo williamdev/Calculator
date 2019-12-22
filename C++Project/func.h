a1019db11b78838aef01577097a869d70346a9a6
William Boycher <williamboycher@hotmail.com>
2019-12-22 12:43:17 -0600

    yes

--- C++Project/func.h
+++ C++Project/func.h
@@ -1,31 +1,194 @@
 #pragma once
-int add(int x, int y)
+void add_Onto(int &past, int &present, int &after, bool &Operate, std::string &setting)
 {
-	int result_Add = x + y;
-	return result_Add;
+	if (setting == "+")
+	{
+		std::cout << "Give another number to add to your previous result, " << past << ":" << std::endl;
+		std::cin >> after;
+		present = past + after;
+		std::cout << present << std::endl;
+		Operate = true;
+		past = present;
+		present = 0;
+	}
 }
-int  subtract(int x, int y)
+void subtract_From(int &past, int &present, int &after, bool &Operate, std::string &setting)
 {
-	int result_Subtract = x - y;
-	return result_Subtract;
+	if (setting == "-")
+	{
+		std::cout << "Give another number to subtract from your previous result, " << past << ":" << std::endl;
+		std::cin >> after;
+		present = past - after;
+		std::cout << present << std::endl;
+		Operate = true;
+		past = present;
+		present = 0;
+	}
 }
-int  multiply(int x, int y)
+void multiply_By(int &past, int &present, int &after, bool &Operate, std::string &setting)
 {
-	int result_Multiply = x * y;				/*
-												pretty self explanatory as these functions are quite short.
-												Just made these to try out parameters, and I don't think they are very useful
-												in the ways of performance/functionality, but they help tidy up the code for people
-												reading it
-												*/
-	return result_Multiply;
+	if (setting == "*")
+	{
+		std::cout << "Give another number to multiply by your previous result, " << past << ":" << std::endl;
+		std::cin >> after;
+		present = past - after;
+		std::cout << present << std::endl;
+		Operate = true;
+		past = present;
+		present = 0;
+	}
 }
-int  divide(int x, int y)
+void divide_By(int &past, int &present, int &after, bool &Operate, std::string &setting)
 {
-	int result_Divide = x / y;
-	return result_Divide;
+	if (setting == "/")
+	{
+		std::cout << "Give another number to divide by your previous result, " << past << ":" << std::endl;
+		std::cin >> after;
+		present = past / after;
+		std::cout << present << std::endl;
+		Operate = true;
+		past = present;
+		present = 0;
+	}
 }
-int modulus(int x, int y)
+void modulus_By(int &past, int &present, int &after, bool &Operate, std::string &setting)
 {
-	int result_Modulus = x % y;
-	return result_Modulus;
+	if (setting == "%")
+	{
+		std::cout << "Give another number to divide by your previous result, " << past << ":" << std::endl;
+		std::cin >> after;
+		present = past / after;
+		std::cout << present << std::endl;
+		Operate = true;
+		past = present;
+		present = 0;
+	}
+}
+void first_Num_Prompt(int &first)
+{
+	std::cout << "Your First Number?" << std::endl; //prompt for first number
+	std::cin >> first; //first number input by user
+	while (std::cin.fail())
+	{
+		std::cout << "Input an integer only with no spaces or characters" << std::endl;
+		std::cin.clear();
+		std::cin.ignore(256, '\n');
+		std::cin >> first;
+	}
+}
+void second_Num_Prompt_add(int& second, int& first, int& past, bool& Operate, std::string setting)
+{
+	if (setting == "+")//addition								// Way too many if statements, maybe could be shortened 
+	{
+		std::cout << "Your Second Number?" << std::endl; //prompts second number			
+		std::cin >> second;
+		while (std::cin.fail())
+		{
+			std::cout << "Input an integer only with no spaces or characters" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(256, '\n');
+			std::cin >> second;
+		}
+		past = first + second;
+		std::cout << past << std::endl;
+		Operate = true;
+	}
+}
+void second_Num_Prompt_subtract(int& second, int& first, int& past, bool& Operate, std::string setting)
+{
+	if (setting == "-")//addition								// Way too many if statements, maybe could be shortened 
+	{
+		std::cout << "Your Second Number?" << std::endl; //prompts second number			
+		std::cin >> second;
+		while (std::cin.fail())
+		{
+			std::cout << "Input an integer only with no spaces or characters" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(256, '\n');
+			std::cin >> second;
+		}
+		past = first - second;
+		std::cout << past << std::endl;
+		Operate = true;
+	}
+}
+void second_Num_Prompt_multiply(int& second, int& first, int& past, bool& Operate, std::string setting)
+{
+	if (setting == "*")//addition								// Way too many if statements, maybe could be shortened 
+	{
+		std::cout << "Your Second Number?" << std::endl; //prompts second number			
+		std::cin >> second;
+		while (std::cin.fail())
+		{
+			std::cout << "Input an integer only with no spaces or characters" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(256, '\n');
+			std::cin >> second;
+		}
+		past = first * second;
+		std::cout << past << std::endl;
+		Operate = true;
+	}
+}
+void second_Num_Prompt_divide(int& second, int& first, int& past, bool& Operate, std::string setting)
+{
+	if (setting == "/")//addition								// Way too many if statements, maybe could be shortened 
+	{
+		std::cout << "Your Second Number?" << std::endl; //prompts second number			
+		std::cin >> second;
+		while (std::cin.fail())
+		{
+			std::cout << "Input an integer only with no spaces or characters" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(256, '\n');
+			std::cin >> second;
+		}
+		past = first / second;
+		std::cout << past << std::endl;
+		Operate = true;
+	}
+}
+void second_Num_Prompt_modulus(int& second, int& first, int& past, bool& Operate, std::string setting)
+{
+	if (setting == "%")//addition								// Way too many if statements, maybe could be shortened 
+	{
+		std::cout << "Your Second Number?" << std::endl; //prompts second number			
+		std::cin >> second;
+		while (std::cin.fail())
+		{
+			std::cout << "Input an integer only with no spaces or characters" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(256, '\n');
+			std::cin >> second;
+		}
+		past = first % second;
+		std::cout << past << std::endl;
+		Operate = true;
+	}
+}
+	
+void operation_Setting_input(std::string &z)
+{
+	std::cout << "+ for add, - for subtract, * for multiply, / for divide, and % for modulus. Type one to choose operator" << std::endl;
+	//prompt for operator
+	std::cin >> z; //input the operator
+	while (std::cin.fail())
+	{
+		std::cout << "Error" << std::endl;
+		std::cin.clear();
+		std::cin.ignore(256, '\n');
+		std::cin >> z;
+	}
+}
+void operation_Switch_Setting_input(std::string &x)
+{
+	std::cout << "Operate onto this result? Y/N" << std::endl;
+	std::cin >> x;
+	while (std::cin.fail())
+	{
+		std::cout << "Error" << std::endl;
+		std::cin.clear();
+		std::cin.ignore(256, '\n');
+		std::cin >> x;
+	}
 }
--- C++Project/main.cpp
+++ C++Project/main.cpp
@@ -12,177 +12,41 @@ int main()
 	int past_Output; //previous output
 	int present_Output; //current output
 	bool didOneOperation = false; //to check if the program can prompt the user to operate on previous result, is set to true after you do one operation with the first_Num and second_Num
-	
-	while (check_Stop > 0) //check_Stop is used to stop the while loop if the user declines the option to add, subtract, multiply, divide, or modulus the previous result by a subsequently given number.
+
+	first_Num_Prompt(first_Num);
+	operation_Setting_input(operation_Setting);
+	while (didOneOperation != true)
 	{
-		std::cout << "Your First Number?" << std::endl; //prompt for first number
-		std::cin >> first_Num; //first number input by user
-		while (std::cin.fail())
+		second_Num_Prompt_add(second_Num, first_Num, past_Output, didOneOperation, operation_Setting);
+		second_Num_Prompt_subtract(second_Num, first_Num, past_Output, didOneOperation, operation_Setting);
+		second_Num_Prompt_divide(second_Num, first_Num, past_Output, didOneOperation, operation_Setting);
+		second_Num_Prompt_multiply(second_Num, first_Num, past_Output, didOneOperation, operation_Setting);
+		second_Num_Prompt_modulus(second_Num, first_Num, past_Output, didOneOperation, operation_Setting);
+    }
+
+	while(didOneOperation == true)
+	{
+		operation_Switch_Setting_input(operation_Switch_Setting);
+
+		if (operation_Switch_Setting == "Y" || operation_Switch_Setting == "y")
 		{
-			std::cout << "Input an integer only with no spaces or characters" << std::endl;
-			std::cin.clear();
-			std::cin.ignore(256, '\n');
-			std::cin >> first_Num;
+			operation_Setting_input(operation_Setting);
+			add_Onto(past_Output, present_Output, after_Num, didOneOperation, operation_Setting);
+			subtract_From(past_Output, present_Output, after_Num, didOneOperation, operation_Setting);
+			multiply_By(past_Output, present_Output, after_Num, didOneOperation, operation_Setting);
+			divide_By(past_Output, present_Output, after_Num, didOneOperation, operation_Setting);
+			modulus_By(past_Output, present_Output, after_Num, didOneOperation, operation_Setting);
 		}
-		std::cout << "+ for add, - for subtract, * for multiply, / for divide, and % for modulus. Type one to choose operator" << std::endl; 
-		//prompt for operator
-		std::cin >> operation_Setting; //input the operator
-		if (didOneOperation == false)
+
+		else if (operation_Switch_Setting == "N" || operation_Switch_Setting == "n")
 		{
-			if (operation_Setting == "+")//addition								// Way too many if statements, maybe could be shortened 
-			{
-				std::cout << "Your Second Number?" << std::endl; //prompts second number			
-				std::cin >> second_Num;
-				while (std::cin.fail())
-				{
-					std::cout << "Input an integer only with no spaces or characters" << std::endl;
-					std::cin.clear();
-					std::cin.ignore(256, '\n');
-					std::cin >> second_Num;
-				}
-				past_Output = add(first_Num, second_Num);
-				std::cout << past_Output << std::endl;
-				didOneOperation = true;
-			}
+			std::cout << "Ok, shutting down...";
+			abort;
+			exit;
+			return 0;
 
-			else if (operation_Setting == "-")//subtraction
-			{
-				std::cout << "Your Second Number?" << std::endl; //prompts second number				// Way too many if checks, maybe could be shortened 
-				std::cin >> second_Num;
-				past_Output = subtract(first_Num, second_Num);
-				std::cout << past_Output << std::endl;
-				didOneOperation = true;
-			}
-
-			else if (operation_Setting == "*")//multiplication						 // Way too many if checks, maybe could be shortened 
-			{
-				std::cout << "Your Second Number?" << std::endl; //prompts second number
-				std::cin >> second_Num;
-				past_Output = multiply(first_Num, second_Num);
-				std::cout << past_Output << std::endl;
-				didOneOperation = true;
-			}
-
-			else if (operation_Setting == "/")//division							 // Way too many if checks, maybe could be shortened 
-			{
-				std::cout << "Your Second Number?" << std::endl; //prompts second number
-				std::cin >> second_Num;
-				past_Output = divide(first_Num, second_Num);
-				std::cout << past_Output << std::endl;
-				didOneOperation = true;
-			}
-
-			else if (operation_Setting == "%")//modulus                // Way too many if checks, maybe could be shortened 
-			{
-				std::cout << "Your Second Number?" << std::endl; //prompts second number
-				std::cin >> second_Num;
-				past_Output = modulus(first_Num, second_Num);
-				std::cout << past_Output << std::endl;	
-				didOneOperation = true;
-			}
-
-			else
-			{
-				std::cout << "Please input an operator";
-			}
 		}
-
-
-
-
-		//NOTE: This next section uses the same logic as before just with different variables
-
-
-
-
-			if(didOneOperation == true)
-			{
-				while (check_Stop > 0)
-				{
-					std::cout << "Operate onto this result? Y/N" << std::endl;
-					std::cin >> operation_Switch_Setting;
-					while (std::cin.fail())
-					{
-						std::cout << "Input Y or N" << std::endl;
-						std::cin.clear();
-						std::cin.ignore(256, '\n');
-						std::cin >> operation_Switch_Setting;
-					}
-					if (operation_Switch_Setting == "Y" || operation_Switch_Setting == "y") //checks for yes option
-					{	
-						std::cout << "+ for add, - for subtract, * for multiply, / for divide, and % for modulus. Type one to choose operator" << std::endl;
-						std::cin >> operation_Setting;
-						if (operation_Setting == "+")                 // Way too many if checks, maybe could be shortened 
-						{
-							std::cout << "Give another number to add to your previous result, " << past_Output << ":" << std::endl;
-							std::cin >> after_Num;
-							present_Output = add(past_Output, after_Num);
-							std::cout << present_Output << std::endl;
-							didOneOperation = true;
-							past_Output = present_Output;
-							present_Output = 0;
-						}
-
-						else if (operation_Setting == "-")          // Way too many if checks, maybe could be shortened 
-						{ 
-							std::cout << "Give another number to subtract from your previous result, " << past_Output << ":" << std::endl;
-							std::cin >> after_Num;
-							present_Output = subtract(past_Output, after_Num);
-							std::cout << present_Output << std::endl;
-							didOneOperation = true;
-							past_Output = present_Output;
-							present_Output = 0;
-						}
-
-						else if (operation_Setting == "*")          // Way too many if checks, maybe could be shortened 
-						{
-							std::cout << "Give another number to multiply your previous result, " << past_Output << ", by:" << std::endl;
-							std::cin >> after_Num;
-							present_Output = multiply(past_Output, after_Num);
-							std::cout << present_Output << std::endl;
-							didOneOperation = true;
-							past_Output = present_Output;
-							present_Output = 0;
-						}
-
-						else if (operation_Setting == "/")          // Way too many if checks, maybe could be shortened 
-						{
-							std::cout << "Give another number to divide your previous result," << past_Output << ", by:" << std::endl;
-							std::cin >> after_Num;
-							present_Output = divide(past_Output, after_Num);
-							std::cout << present_Output << std::endl;
-							didOneOperation = true;
-							past_Output = present_Output;
-							present_Output = 0;
-						}
-
-						else if (operation_Setting == "%")          // Way too many if checks, maybe could be shortened 
-						{
-							std::cout << "Give another number to modulus your previous result," << past_Output << ", by:" << std::endl;
-							std::cin >> after_Num;
-							present_Output = modulus(past_Output, after_Num);
-							std::cout << present_Output << std::endl;
-							didOneOperation = true;
-							past_Output = present_Output;
-							present_Output = 0;
-						}
-						else
-						{
-							std::cout << "Please input an operator";
-						}
-						
-				}
-					else if (operation_Switch_Setting == "N" || operation_Switch_Setting == "n")
-					{
-						std::cout << "Ok, shutting down...";
-						abort;
-						exit;
-						return 0;
-
-					}
-				}
-		}
-
-
 	}
 }
+
+
